017938e6820e7c50c0e6430ca34adebaaec28297
Will Holt <halbrd@gmail.com>
2018-09-11 23:34:43 +1000

    implement file globbing and sorting

--- lightrenamer.py
+++ lightrenamer.py
@@ -2,10 +2,13 @@ import argparse
 import json
 import requests
 import os
-
-api = lambda path: 'https://api.thetvdb.com/' + path
+from glob import glob
+import re
 
 CREDENTIALS_FILE = 'lightrenamer-credentials.json'
+
+api = lambda path: 'https://api.thetvdb.com/' + path
+std_headers = {'Content-Type': 'application/json'}
 
 def get_auth_jwt(args):
 
@@ -38,14 +41,40 @@ def get_auth_jwt(args):
 
     return credentials['jwt']
 
+def process_files(files):
+    organized_files = {}
+
+    for file_name in files:
+        file = file_name
+        file = file.replace('.', ' ').split()
+
+        # get index of term that contains episode index
+        episode_index_index = None
+        for i, term in enumerate(file):
+            if re.fullmatch('[Ss]?\d?\d[Eex]\d\d', term):
+                episode_index_index = i
+        if episode_index_index is None:
+            raise ValueError('no valid season/episode index found')
+
+        show_name = ' '.join(file[:episode_index_index])
+        episode_index = file[episode_index_index]
+
+        if not show_name in organized_files:
+            organized_files[show_name] = {}
+        organized_files[show_name][episode_index] = file_name
+
+    return organized_files
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--apikey', help='thetvdb api token (only needed the first time)')
+    parser.add_argument('pattern', help='regexp to match files to rename')
     args = parser.parse_args()
 
-    jwt = get_auth_jwt(args)
-    print(jwt)
+    std_headers['jwt'] = get_auth_jwt(args)
 
+    files = glob('*' + args.pattern + '*')
 
+    print(process_files(files))
